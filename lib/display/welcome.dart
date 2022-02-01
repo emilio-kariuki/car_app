d6b5b7c78ec896b8da4f063327abd321723c0222
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-01 11:32:14 +0300

    February 1, 2022 11:32 AM

--- lib/display/welcome.dart
+++ lib/display/welcome.dart
@@ -2,6 +2,7 @@
 
 import 'package:car_app/Constants/constants.dart';
 import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 
 class Welcome extends StatefulWidget {
@@ -34,6 +35,14 @@ class _WelcomeState extends State<Welcome> {
               backgroundColor: kprimaryColor,
               child: Icon(Icons.arrow_right_rounded,size:40,color:Colors.white),
               radius: 30,
+            )
+          ),
+          Positioned(
+            bottom: 30,
+            right: 20,
+            child: Text(
+              "Car",
+              style: GoogleFonts.roboto(fontSize: 30,color:Colors.white,fontWeight: FontWeight.bold)
             )
           )
         ],
