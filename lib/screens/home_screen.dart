8734785e530bdf6dd9d514e6121ab5d7e33a8b80
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-01 12:49:00 +0300

    February 1, 2022 12:49 PM

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,13 +12,55 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: kaccentColor,
-        items: [
-          
-        ],
-      )
+        bottomNavigationBar: BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: kaccentColor,
+      items: [],
+    ));
+  }
+}
+
+class BuildBottomNavigationBar extends StatefulWidget {
+  @override
+  _BuildBottomNavigationBarState createState() =>
+      _BuildBottomNavigationBarState();
+}
+
+class _BuildBottomNavigationBarState extends State<BuildBottomNavigationBar> {
+  List<String> buttomNav = [
+    'assets/icons/app.png',
+    'assets/icons/calendar.png',
+    'assets/icons/pin.png',
+    'assets/icons/user.png',
+  ];
+
+  int selecTedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 18),
+      color: kaccentColor,
+      height: 65,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: buttomNav.length,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            setState(() {
+              selecTedIndex = index;
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(right: kpadding * 2, left: kpadding),
+            child: Image.asset(
+              buttomNav[index],
+              height: 40,
+              color: selecTedIndex == index ? kbottonColor : Colors.grey,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
