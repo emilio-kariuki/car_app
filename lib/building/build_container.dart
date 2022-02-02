c4116734cdf95c047d1e7ab05cf3161a704ded63
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-02 20:19:14 +0300

    February 2, 2022 8:19 PM

--- lib/building/build_container.dart
+++ lib/building/build_container.dart
@@ -1,13 +1,22 @@
+import 'package:car_app/Constants/constants.dart';
 import "package:flutter/material.dart";
 
-
 class BuildContainer extends StatelessWidget {
-  const BuildContainer({Key? key}) : super(key: key);
+  final Color color;
+  final Widget child;
+  const BuildContainer({required this.color,required this.child,Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: kaccentColor, width: 1.0),
+      ),
+      child: child,
     );
   }
 }
