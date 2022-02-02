233bb6e480d2eba15caf2a4febe891f1fbec1f1a
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-02 20:25:47 +0300

    February 2, 2022 8:25 PM

---
+++ lib/building/build_appBar.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class BuildAppBar extends StatelessWidget {
+  const BuildAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
