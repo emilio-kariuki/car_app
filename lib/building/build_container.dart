52f14dfd1b40e94f107a029e437c72317db4d9e6
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-02 20:15:14 +0300

    February 2, 2022 8:15 PM

---
+++ lib/building/build_container.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+
+class BuildContainer extends StatelessWidget {
+  const BuildContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
